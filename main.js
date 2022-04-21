487dbc6000190399b406616661e2230f62c54200
Zoran Duric <zordjuric@gmail.com>
2022-04-21 12:33:11 +0300

    inita

--- main.js
+++ main.js
@@ -1,20 +1,74 @@
-import { listOfQuestions } from "./data.js";
-console.log(listOfQuestions);
+// import { listOfQuestions } from "./data.js";
+
+let listOfQuestions = [
+  {
+    id: 1,
+    question: "What is capital of Finland?",
+    answer: "helsinki",
+  },
+  {
+    id: 2,
+    question: "What is capital of Germany?",
+    answer: "berlin",
+  },
+  {
+    id: 3,
+    question: "What is capital of France?",
+    answer: "paris",
+  },
+  {
+    id: 4,
+    question: "What is capital of Spain?",
+    answer: "madrid",
+  },
+  {
+    id: 5,
+    question: "What is capital of Portugal?",
+    answer: "lisbon",
+  },
+  {
+    id: 6,
+    question: "What is capital of Hungary?",
+    answer: "budapest",
+  },
+  {
+    id: 7,
+    question: "What is capital of Moldava?",
+    answer: "chisinau",
+  },
+];
 
 const initApp = () => {
   console.log("App init");
   loadScore();
+  clearDisplay();
   getQuestion();
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
 let score = 0;
-let pastQuestions = [];
-let randomQuesiton;
+let pastQuestions = [0];
+
+let zoki = [1];
+let mirko = { id: 1 };
+
+console.log("--------------");
+console.log(zoki === mirko.id);
+console.log(JSON.stringify(mirko.id));
+console.log(Object.values(mirko));
+let traktor = JSON.stringify(mirko.id);
+let kam = JSON.stringify(zoki);
+console.log(typeof traktor);
+console.log(typeof kam);
+
+console.log("--------------");
+
+let randomQuesiton = { id: -1 };
+
 const loadScore = () => {
   const scoreSpan = document.getElementById("score");
   scoreSpan.textContent = score;
-  if (score === 10) {
+  if (score === 7) {
     score = 0;
   }
 };
@@ -33,28 +87,34 @@ const updateQuestionList = () => {
     let updateListOfQuestions = listOfQuestions.filter(
       (question) => question.id !== randomQuesiton.id
     );
-
+    listOfQuestions = updateListOfQuestions;
     console.log(updateListOfQuestions);
   }
 };
 
 const getQuestion = () => {
   console.log(listOfQuestions);
+
   if (listOfQuestions.length > 0) {
     console.log("if statment");
-    //  var question = Math.floor(Math.random() * listOfQuestions.length);
+
     randomQuesiton =
       listOfQuestions[Math.floor(Math.random() * listOfQuestions.length)];
     pastQuestions = [...pastQuestions, randomQuesiton.id];
-  }
 
-  updateQuestionList();
-  displayQuestion(randomQuesiton);
+    updateQuestionList();
+    displayQuestion(randomQuesiton);
+  }
+};
+const container = document.getElementById("listQuestions");
+
+const clearDisplay = () => {
+  container.innerHTML = "";
 };
 
 const displayQuestion = () => {
   console.log(pastQuestions);
-  const container = document.getElementById("listQuestions");
+
   const div = document.createElement("div");
   const questionText = document.createElement("p");
   const labelAnswer = document.createElement("label");
@@ -89,10 +149,13 @@ const processAnswer = (event) => {
   if (capitalAnswer === randomQuesiton.answer) {
     increment();
     console.log("radi");
-
+    clearDisplay();
     getQuestion();
   } else {
-    getQuestion();
     console.log("wrong answer");
+  }
+  if (listOfQuestions.length <= 0) {
+    console.log("TESTEEEEEET");
+    initApp();
   }
 };
